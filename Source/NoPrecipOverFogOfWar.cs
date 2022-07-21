987db814ee41ac188a08ce230f1e772c8eb9f2a0
SaberShip <donotsent@gmail.com>
2022-07-20 23:09:57 -0500

    Better mask rain around fog of war edges

--- Source/NoPrecipOverFogOfWar.cs
+++ Source/NoPrecipOverFogOfWar.cs
@@ -33,33 +33,77 @@ namespace NoPrecipOverFogOfWar
             return AccessTools.Method(AccessTools.TypeByName("SectionLayer_IndoorMask"), "HideRainPrimary");
         }
 
-        public static void Prefix(IntVec3 c)
+        public static void Prefix(IntVec3 c, ref IntVec3 __state)
         {
+            __state = c;
             CheckingRainMask = true;
         }
 
-        public static void Postfix(ref bool __result)
+        public static void Postfix(IntVec3 __state, ref bool __result)
         {
             CheckingRainMask = false;
-            if (IsFoggedForPrecipitation.MaskRain)
+            if (!__result)
             {
-                IsFoggedForPrecipitation.MaskRain = false;
-                __result = true;
+                if (IsFoggedForPrecipitation.IsInFog)
+                {
+                    IsFoggedForPrecipitation.IsInFog = false;
+                    __result = true;
+                }
+                else if (!GridsUtilityRoofedPatch.Roofed)
+                {
+                    Building b = __state.GetEdifice(GridsUtilityRoofedPatch.TileMap);
+                    
+                    if (b != null && b.def.holdsRoof && !b.def.MadeFromStuff)
+                    {
+                        __result = true;
+                        GridsUtilityEdificePatch.Building = null;
+                    }
+                }
             }
         }
     }
 
     [HarmonyPatch(typeof(FogGrid), nameof(FogGrid.IsFogged), typeof(IntVec3))]
-    public static class IsFoggedForPrecipitation
+    public class IsFoggedForPrecipitation
     {
 
-        public static bool MaskRain = false;
+        public static bool IsInFog = false;
 
         public static void Postfix(ref bool __result)
         {
             if (PrecipitationPatch.CheckingRainMask && __result)
             {
-                MaskRain = true;
+                IsInFog = true;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(GridsUtility), nameof(GridsUtility.Roofed))]
+    public class GridsUtilityRoofedPatch
+    {
+        public static bool Roofed = false;
+        public static Map TileMap = null;
+
+        public static void Postfix(Map map, ref bool __result)
+        {
+            if (PrecipitationPatch.CheckingRainMask)
+            {
+                TileMap = map;
+                Roofed = __result;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(GridsUtility), nameof(GridsUtility.GetEdifice))]
+    public class GridsUtilityEdificePatch
+    {
+        public static Building Building = null;
+
+        public static void Postfix(ref Building __result)
+        {
+            if (PrecipitationPatch.CheckingRainMask)
+            {
+                Building = __result;
             }
         }
     }
